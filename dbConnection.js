f67183ca8ad87160bd1cdb0d50fb766e1d4fec4d
Bhupendra211 <bhupendrabisht488@gmail.com>
2024-07-06 12:07:20 +0530

    update database connection

--- dbConnection.js
+++ dbConnection.js
@@ -2,17 +2,19 @@ const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DBNAME, "root" || process.env.USERNAME, process.env.PASSWORD, {
+// Connection for Localhost
+// const sequelize = new Sequelize(process.env.DBNAME, "root" || process.env.USERNAME, process.env.PASSWORD, {
+//     host: process.env.HOST,
+//     dialect: 'mysql',
+//     port: process.env.DB_PORT || 3307
+// });
+
+// Connection for Aiven
+const sequelize = new Sequelize(process.env.DBNAME, "avnadmin" || process.env.USERNAME, process.env.PASSWORD, {
     host: process.env.HOST,
     dialect: 'mysql',
-    port: process.env.DB_PORT || 3307
+    port: 25853
 });
-
-// const sequelize = new Sequelize("defaultdb", "avnadmin", "AVNS_yWjVxyX3kAX5cOly0b8", {
-//     host: "mysql-208c69f9-bhupendrabisht488-99eb.l.aivencloud.com",
-//     dialect: 'mysql',
-//     port: 25853
-// });
 
 const connectDB = async () => {
     try {
